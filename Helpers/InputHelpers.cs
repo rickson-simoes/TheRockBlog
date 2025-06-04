8bc218293cbfd12d41f9a5545f71d9e90b29bcef
Rickson Simoes <rickson.simoes@hotmail.com>
2025-06-04 10:45:03 -0300

    helpers - text

--- Helpers/InputHelpers.cs
+++ Helpers/InputHelpers.cs
@@ -7,7 +7,7 @@
         /// <para> - On the param is the field name that will pop up for the user.</para>
         /// </summary>
         /// <param name="field">Name of the param to pop on the screens.</param>
-        /// <returns>A valid string that is not null or with white spaces.</returns>
+        /// <returns>A valid string that is not null or with only white spaces.</returns>
         public static string NotNullOrWhiteSpace(string field)
         {
             Console.Write($"\n{field}: ");
